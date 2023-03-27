59315de99a6654494ea6444fda5ec53afdfff3e1
Anthorra <molnar87gabor@gmail.com>
2023-03-27 17:10:35 +0200

    not sure

--- src/main/java/com/anthorra/moneyapp/PrimaryController.java
+++ src/main/java/com/anthorra/moneyapp/PrimaryController.java
@@ -7,7 +7,9 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.ResourceBundle;
+import java.util.Set;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -59,21 +61,20 @@ public class PrimaryController implements Initializable
     public void initialize(URL url, ResourceBundle rb)
     {
         sm = new StatusMessage(txtStatus);
+        frtList = queryTypeList();
         
         initInputPage();
         initInputTypePage();
         
     }
+    
+    /*PAGE 1*/
     public void initInputPage()
     {
         cbType.setPromptText("- Válassz -");
         cbSubType.setPromptText("- Válassz -");
         
-        
-        String[] types = new String[]{"valami", "még valami"};
-        cbType.getItems().addAll(types);
-        cbSubType.getItems().addAll(types);
-        
+        refreshInputPageTypes();
         
         ToggleGroup rbtnToggleGroup = new ToggleGroup();
         rbtnExpense.setToggleGroup(rbtnToggleGroup);
@@ -82,25 +83,49 @@ public class PrimaryController implements Initializable
                 
         inpDatePicker.setValue(LocalDate.now());
     }
+    public void refreshInputPageTypes()
+    {
+        cbType.getItems().clear();
+        
+        for(int i = 0; i < frtList.size(); i++)
+        {
+            if(i==0 || frtList.get(i).getMainId()!=frtList.get(i-1).getMainId())
+            {
+                cbType.getItems().add(frtList.get(i).getMainDesc());
+            }
+        }
+    }
+    @FXML
+    public void refreshInputPageSubType()
+    {
+        cbSubType.getItems().clear();
+        
+        for(int i = 0; i < frtList.size(); i++)
+        {
+            if(frtList.get(i).getMainDesc().equals(cbType.getValue()))
+            {
+                cbSubType.getItems().add(frtList.get(i).getSubDesc());
+            }
+        }
+    }
+    public void clearInputPage()
+    {
+        rbtnExpense.setSelected(true);
+        inpAmount.clear();
+        cbType.getSelectionModel().clearSelection();
+        cbSubType.getSelectionModel().clearSelection();
+        //inpDatePicker.setValue(LocalDate.now());
+        inpComment.clear();
+    }
     
+    /*PAGE 2*/
     public void initInputTypePage()
     {
-        /*FinancialRecordTypes frt = new FinancialRecordTypes(1, "maint1", 2, "subt1");
-        frtList.add(frt);
-        frt = new FinancialRecordTypes(1, "maint1", 3, "subt2");
-        frtList.add(frt);
-        frt = new FinancialRecordTypes(2, "maint2", 4, "subt4");
-        frtList.add(frt);
-        frt = new FinancialRecordTypes(2, "maint2", 5, "subt5");
-        frtList.add(frt);*/
-        frtList = queryTypeList();
-        
         treeTableTypeId.setCellValueFactory(new TreeItemPropertyValueFactory<>("mainId"));
         treeTableTypeColumn.setCellValueFactory(new TreeItemPropertyValueFactory<>("mainDesc"));
         treeTableTypeSubTypeId.setCellValueFactory(new TreeItemPropertyValueFactory<>("subId"));
         treeTableSubTypeColumn.setCellValueFactory(new TreeItemPropertyValueFactory<>("subDesc"));
         
-                
         TreeItem top = new TreeItem(new FinancialRecordTypes(0,"Kategóriák",0,""));
         
         TreeItem ti, ti2;
@@ -110,6 +135,7 @@ public class PrimaryController implements Initializable
             {
                 ti2 = new TreeItem(new FinancialRecordTypes(frtList.get(i).getMainId(),frtList.get(i).getMainDesc(),0,""));
                 top.getChildren().add(ti2);
+                ti2.setExpanded(true);
                 
                 for(int j = i; j < frtList.size() && frtList.get(i).getMainId()==frtList.get(j).getMainId(); j++)
                 {
@@ -118,14 +144,89 @@ public class PrimaryController implements Initializable
                 }
             }
         }
-        
+        top.setExpanded(true);
         treeTableTypes.setRoot(top);
+    }
+    
+    public void refreshInputTypePage()
+    {
+        frtList = queryTypeList();
+        
+        TreeItem top = treeTableTypes.getRoot();
+        top.getChildren().clear();
+        
+        TreeItem ti, ti2;
+        for(int i = 0; i < frtList.size(); i++)
+        {
+            if(i==0 || frtList.get(i).getMainId()!=frtList.get(i-1).getMainId())
+            {
+                ti2 = new TreeItem(new FinancialRecordTypes(frtList.get(i).getMainId(),frtList.get(i).getMainDesc(),0,""));
+                top.getChildren().add(ti2);
+                //ti2.setExpanded(true);
+                
+                for(int j = i; j < frtList.size() && frtList.get(i).getMainId()==frtList.get(j).getMainId(); j++)
+                {
+                    ti = new TreeItem(frtList.get(j));
+                    ti2.getChildren().add(ti);
+                }
+            }
+        }
+        treeTableTypes.setRoot(top);
+    }
+    
+    
+    /* BACKGROUND PROCESSES */
+    /**
+     * 
+     * @param type integer -> 1 MainType Desc, 2 if SubType
+     * @param desc Description text from the combobox on Page1
+     * @return int is the DB ID for that category to store
+     */
+    public int retrieveTypeIdByDesc(int type, String desc)
+    {
+        int ret_val = -1;
+        
+        switch(type)
+        {
+            case 1:
+                for(int i = 0; i < frtList.size(); i++)
+                {
+                    if(frtList.get(i).getMainDesc().equals(cbType.getValue()))
+                    {
+                        ret_val = frtList.get(i).getMainId();
+                    }
+                }
+                break;
+            case 2:
+                for(int i = 0; i < frtList.size(); i++)
+                {
+                    if(frtList.get(i).getSubDesc().equals(cbSubType.getValue()))
+                    {
+                        ret_val = frtList.get(i).getSubId();
+                    }
+                }
+                break;
+        }
+        return ret_val;
     }
     
     
     
     
-    /* PROCESSES */
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /* UI PROCESSES */
         /* Submit */
     @FXML public void submitFinancialRecord()
     {
@@ -133,12 +234,15 @@ public class PrimaryController implements Initializable
         
         Date realizedDate = Date.from(inpDatePicker.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant()); //Date.from(inpDatePicker.getValue());
         String datum = inpDatePicker.getValue().toString();
+        int typeId = retrieveTypeIdByDesc(1, cbType.getValue());
+        int subTypeId = retrieveTypeIdByDesc(2, cbSubType.getValue());
+        
         
         double amount = Integer.parseInt(inpAmount.getText());
         boolean isExpense = rbtnExpense.isSelected();
-        FinancialRecord fr = new FinancialRecord(amount, isExpense, cbType.getSelectionModel().getSelectedIndex(), cbSubType.getSelectionModel().getSelectedIndex(),inpComment.getText(),datum);
-        DBHandler.insertIntoDB(fr, sm);
-        
+        //FinancialRecord fr = new FinancialRecord(amount, isExpense, cbType.getSelectionModel().getSelectedIndex(), cbSubType.getSelectionModel().getSelectedIndex(),inpComment.getText(),datum);
+        DBHandler.insertIntoDB(new FinancialRecord(amount, isExpense, typeId, subTypeId,inpComment.getText(),datum), sm);
+        clearInputPage();
     }
 
          /* Add Type */   
@@ -146,7 +250,18 @@ public class PrimaryController implements Initializable
     @FXML Button btnInpType;
     @FXML public void submitTypeRecord()
     {
+        /*VALIDATION!*/
+        
         DBHandler.insertTypeIntoDB(inpType.getText(), sm);
-        /*HOZZÁADNI A LISTÁHOZ IS ÉS SORBA RENDEZNI*/
+        refreshInputTypePage();
+        refreshInputPageTypes();
+        refreshInputPageSubType();
+        inpType.clear();
+        /*frissiteni minden oldalt ami most vagy majd érintett*/
     }
+    
+    
+   
+    
+    
 }
